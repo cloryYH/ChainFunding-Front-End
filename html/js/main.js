230b43ee78be4bef51fb5926896f7d3ad4e23936
cloryYH <yuehaozhao075@gmail.com>
2022-11-15 02:22:56 +0800

    better login function and simple usermenu

--- html/js/main.js
+++ html/js/main.js
@@ -8,3 +8,19 @@ $("#bars1").click(function(){
     }
     t=1-t;
 });
+
+/*在網頁加載后讀取cookie 決定是顯示登入按鈕還是用戶*/
+
+$(document).ready(function () {
+    var ca=document.cookie.split(';');
+    if(ca.length>1){
+        acc=ca[1].split('=')[1];
+        if (acc!=null){
+            $('#main-user-bt').append("<a href=\"./usermenu.html\">\
+                <i class=\"fa-solid fa-user\" style=\"color:skyblue;\"></i>\個人中心</a>\
+                <div class=\"dropdown-content\"><a href = \"javascript:void(0);\" onclick =\"Logout()\">登出</a></div>");}
+    }
+    else{
+        $('#main-user-bt').append("<a href=\"./login.html\"><i class=\"fa-solid fa-user-plus\" style=\"color:grey;\"></i>登入</a>");
+    }
+});
